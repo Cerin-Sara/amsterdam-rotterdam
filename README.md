4b9a0e3682eae4076510bf8d1f71e97eeedd69b7
Cerin-Sara <saracerin96@gmail.com>
2022-08-03 19:05:19 +0530

    Added the readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# The line graph of day1
+![day_1.png](day_1.png)
--- line.py
+++ line.py
@@ -1,9 +1,14 @@
 from turtle import color
 from bokeh.plotting import figure, show
+import numpy as np
+import pandas as pd
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [100, 200, 300, 400, 500]
-y1 = [250, 100, 300, 500, 100]
+df=pd.read_csv('graph.csv')
+day_1_a=df['DAY1'][0]
+day_1_r=df['DAY1'][1]
+x = [1, 2]
+y = [day_1_a, 200]
+y1 = [day_1_r, 100]
 
 # create a new plot with a title and axis labels
 p = figure(title="Line trial", x_axis_label='Days', y_axis_label='Price per square metre')
